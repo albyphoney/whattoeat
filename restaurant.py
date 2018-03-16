45634632ac11cbb6fe31657807e60494a9f7536c
Emilie Gao <emiliegao@berkeley.edu>
2018-03-16 14:26:42 -0700

    Added 10 test restaurants and some skeleton code for parsing data

---
+++ restaurant.py
@@ -0,0 +1,27 @@
+class Restaurant:
+    def __init__(self, rest_items):
+    	self.ID = rest_id
+
+    def get_id(self):
+    	"""Returns the restaurant ID"""
+    	return self.restaurant_id
+    
+    def get_name(self):
+    	"""Returns the restaurant name"""
+    	return self.name
+    
+    def get_items(self):
+    	"""Returns the restaurant items"""
+    	return self.items
+    
+    def get_avg_rating(self):
+    	"""Returns the average restaurant rating"""
+    	return self.avg_rating
+
+    def get_reviews(self):
+    	"""Adds review to list of reviews for the restaurant"""
+    	self.restaurant_reviews.add(review)
+
+
+
+
