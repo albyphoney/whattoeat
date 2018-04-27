8e19cea548e26110147980dc233c4781c585cecf
Christine Lee <christinelee@berkeley.edu>
2018-04-26 17:18:39 -0700

    restaurants

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from parse_reviews import parseRestaurantItems
 from menu_review_filter import menu_filter,review_filter
+import csv
 
 def track_menu_items(menu_distinct, word_to_menu, reviews_clean_wnl, review_ratings):
 	menu_info = {}
@@ -17,7 +18,7 @@ def track_menu_items(menu_distinct, word_to_menu, reviews_clean_wnl, review_rati
 				menu_info[menu_hit][1] += review_ratings[i]
 	for menu_item in menu_info:
 		if menu_info[menu_item][0] != 0:
-			menu_info[menu_item][1] = menu_info[menu_item][1] / menu_info[menu_item][0]
+			menu_info[menu_item][1] = round(menu_info[menu_item][1] / menu_info[menu_item][0], 1)
 	return menu_info
 
 restaurant_list = parseRestaurantItems()
@@ -28,6 +29,9 @@ for restaurant in restaurant_list:
 	menu_info = track_menu_items(menu_distinct, word_to_menu, reviews_clean_wnl, restaurant.review_ratings)
 	top_ten_ordered = sorted(menu_info.items(), key=lambda x: x[1], reverse=True)[:10]
 	#top_ten_reviewed = sorted(menu_info.items(), key=lambda x: x[1][1], reverse=True)[:10]
+	with open('restaurant.csv', 'a') as csvfile:
+		restaurant_writer = csv.writer(csvfile)
+		restaurant_writer.writerow([restaurant.get_id(), restaurant.get_name(), top_ten_ordered])
 	print(top_ten_ordered)
 	print("%n")
 
